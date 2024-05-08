332cff13ab5a159f97d954c7f2eade99860f5b5b
Nils Jeisecke <nils.jeisecke@saltation.com>
2024-05-08 08:35:09 +0200

    simple testcase

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::time::Instant;
+
+fn bench(name: &str, f: fn()) {
+    const RUN_COUNT: usize = 30;
+
+    let start = Instant::now();
+
+    for _ in 0..RUN_COUNT {
+        f();
+    }
+
+    println!("{} {:?}", name, start.elapsed() / (RUN_COUNT as u32));
+}
+
+fn test_foo() {
+    let (tx, rx) = std::sync::mpsc::sync_channel(5);
+
+    let handle = std::thread::spawn(move || {
+        let mut acc: i64 = 0;
+        while let Ok(value) = rx.recv() {
+            acc += value;
+        }
+        acc
+    });
+
+    for v in 0..100_000 {
+        tx.send(v).unwrap();
+    }
+    drop(tx);
+
+    let sum = handle.join().unwrap();
+
+    println!("sum {}", sum);
+}
+
 fn main() {
-    println!("Hello, world!");
+    bench("foo", test_foo);
 }
